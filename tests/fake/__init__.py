d23e3dcf84aa83776c396bee5c8f471a034e4536
tmknom <tmk.nom@gmail.com>
2026-04-06 08:16:24 +0900

    docs(fake): require-empty-test-init を抑制する理由を明記する

--- tests/fake/__init__.py
+++ tests/fake/__init__.py
@@ -1,5 +1,8 @@
 """共有 Fake 実装パッケージ"""
 
+# Fake は他のテストパッケージからインポートされるライブラリとして機能するため、
+# __init__.py で re-export を行う必要があり、空ファイルにできない。
+# paladin: ignore-file[require-empty-test-init]
 from tests.fake.fs import ErrorFsReader, InMemoryFsReader
 from tests.fake.rule import FakeMultiFileRule, FakePreparableRule, FakeRule
 
